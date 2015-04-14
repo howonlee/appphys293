87a0e9b9d17915a33391c0fa94ddff248baf2492
Howon Lee <howon@howonlee.com>
2015-04-13 19:18:47 -0700

    because I am a terrible person

--- bm.py
+++ bm.py
@@ -1,1 +1,9 @@
-#implement normal and then gossip boltzmann machine
+import numpy as np
+
+if __name__ == "__main__":
+    #an old fun trick:
+    #words are nodes
+    #bigrams are edges
+    #bam! complex network
+    with open("corpus.txt", "r") as corpus_file:
+
